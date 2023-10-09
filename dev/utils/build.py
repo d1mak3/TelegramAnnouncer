6c9383725a3e648ad5fb53d09087c81853e01761
Dmitry Kuimov <d1mak3@yandex.ru>
2023-10-09 21:45:24 +0300

    add installer

--- dev/utils/build.py
+++ dev/utils/build.py
@@ -1,6 +1,6 @@
 from glob import iglob
 import os
-import shutil
+import sys
 
 
 def merge_file(file_path, result_string):
@@ -24,18 +24,21 @@ def safe_write_in_file(file_path, file_name, data):
         file.write(data)
 
 
-build_directory = "../../build/"
+src_directory = sys.argv[1]
+
+if not src_directory:
+    raise "No path to src directory"
+
+build_directory = src_directory + "../"
 result_file_name = "TelegramAnnouncer.py"
-database_providers_directory = "../../src/database_providers/*"
-service_providers_directory = "../../src/services/*"
-clients_directory = "../../src/clients/*"
-enums_directory = "../../src/core/enums/*"
-types_directory = "../../src/core/types/*"
-menu_directory = "../../src/core/menu/*"
-runners_directory = "../../src/core/runners/**/*"
-main_path = "../../src/main.py"
-requirements_path = "../requirements.txt"
-license_path = "../../LICENSE"
+database_providers_directory = src_directory + "database_providers/*"
+service_providers_directory = src_directory + "services/*"
+clients_directory = src_directory + "clients/*"
+enums_directory = src_directory + "core/enums/*"
+types_directory = src_directory + "core/types/*"
+menu_directory = src_directory + "core/menu/*"
+runners_directory = src_directory + "core/runners/**/*"
+main_path = src_directory + "main.py"
 
 result = ""
 
@@ -83,9 +86,8 @@ for path in iglob(runners_directory, recursive=True):
 
     result = merge_file(path, result)
 
-result = merge_file(main_path, result)
+result = merge_file(src_directory + "main.py", result)
+result = result.replace("..", ".")
 safe_write_in_file(build_directory, result_file_name, result)
-shutil.copy(requirements_path, build_directory + "requirements.txt")
-shutil.copy(license_path, build_directory + "LICENSE")
 
 print("Build is finished successfully")
